f93e164362e7b0cac1ff3c7d9b99d1f1448aaae5
D-Hallock223 <dhallock223@gmail.com>
2017-10-29 10:33:38 -0700

    Initial Commit

---
+++ GPSDataCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSDataCollector
+//
+//  Created by AKIL KUMAR THOTA on 10/29/17.
+//  Copyright © 2017 AKIL KUMAR THOTA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
